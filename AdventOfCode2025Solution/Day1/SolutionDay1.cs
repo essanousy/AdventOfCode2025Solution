d044e22bb49e06e004ef83c7ce55e3fce3a4e073
essanousy <mohamed.essanousy@gmail.com>
2025-12-06 18:34:41 +0100

    Add project files.

---
+++ AdventOfCode2025Solution/Day1/SolutionDay1.cs
@@ -0,0 +1,48 @@
+﻿namespace AdventOfCode2025Solution.Day1
+{
+    public static class SolutionDay1
+    {
+        public static int Solve()
+        {
+            int solution = 0;
+            string[] inputLines = System.IO.File.ReadAllLines("Day1/Input.txt");
+            int dialPosition = 50;
+            int start = 0;
+            int end = 99;
+            Console.WriteLine($"The dial starts by pointing at {dialPosition}");
+            foreach (var line in inputLines)
+            {
+
+                string rotation = line.Substring(0, 1);
+                int length = int.Parse(line.Substring(1));
+                
+                if (rotation == "L")
+                {
+                   for (int i = 0; i < length; i++)
+                   {
+                        dialPosition--;
+                        if (dialPosition < start) dialPosition = end;
+                   }
+
+                }
+                else if (rotation == "R")
+                {
+                     for (int i = 0; i < length; i++)
+                    {
+                        dialPosition++;
+                        if (dialPosition > end) dialPosition = start;
+                    }
+                }
+
+                if (dialPosition == 0)
+                {
+                    solution++;
+                }
+
+                Console.WriteLine($"The dial is rotated {line} to point at {dialPosition}");
+            }
+
+            return solution;
+        }
+    }
+}
---
+++ AdventOfCode2025Solution/Program.cs
@@ -0,0 +1,4 @@
+﻿using AdventOfCode2025Solution.Day1;
+
+int solution = SolutionDay1.Solve();
+Console.WriteLine($"Solution for Day 1: {solution}");
